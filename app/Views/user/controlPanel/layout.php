4649fea9cb298f960bab00285d0595b778c65724
Nikola Ubavic <mm16310@alas.matf.bg.ac.rs>
2021-02-14 00:02:38 +0100

    Unapredjen meni u k. panelu

--- app/Views/user/controlPanel/layout.php
+++ app/Views/user/controlPanel/layout.php
@@ -1,14 +1,19 @@
+<?php 
+    $uri = service('uri');
+    $uri->setSilent();
+    $color = 'style="background-color: var(--color-gray-4)"';
+?>
 <main>
 	<h1>Кориснички контролни панел</h1>
 	<div class="controlPanel">
         <nav>
-        	<a href="/user/settings">Подешавања</a>
-        	<a href="/user/exams">Креирани рокови</a>
-        	<a href="/user/saved">Сачувани рокови</a>
+        	<a href="/user/settings" <?=($uri->getSegment(2) == 'settings' ? $color : null)?> >Подешавања</a>
+        	<a href="/user/exams" <?=($uri->getSegment(2) == 'exams' ? $color : null)?> >Креирани&nbsp;рокови</a>
+        	<a href="/user/saved" <?=($uri->getSegment(2) == 'saved' ? $color : null)?> >Сачувани&nbsp;рокови</a>
             <?php if (session()->get('can_manage_users')): ?>
                 <div>Aдминистрација</div>
-                <a href="/user/all">Корисници</a>
-                <a href="/user/log">Лог</a>
+                <a href="/user/all" <?=($uri->getSegment(2) == 'all' ? $color : null)?> >Корисници</a>
+                <a href="/user/log" <?=($uri->getSegment(2) == 'log' ? $color : null)?> >Лог</a>
             <?php endif; ?>
         	<div>Остало</div>
             <a href="/tools">Алати</a>
