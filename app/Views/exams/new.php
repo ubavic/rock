124359563f0f4f60db1789b8892d118908aa2b26
Nikola Ubavić <mm16310@alas.matf.bg.ac.rs>
2021-05-03 01:07:56 +0200

    Dodat link ka uputstvu Uputstvo revidirano

--- app/Views/exams/new.php
+++ app/Views/exams/new.php
@@ -5,65 +5,65 @@
 	<?php else: ?>
 		<h1>Измени рок</h1>
 	<?php endif;?>
-		<form style="margin: 1em 0;" method="post" id="form">
-			<div class="formRow">
-				<div class="formRowElement">
-					<label for="subject">Предмет:</label>
-				</div>
-				<select name="subject" id="subject" class="formRowElement">
-					<?= $subjectsList ?>
-				</select>
-				<div style="margin-left: auto"></div>
-				<label for="date" class="formRowElement">Датум:</label>
-				<input type="date" name="date" id="date" value="<?= $exam->date ?>">
+	<p>Ако нисте упознати са начином додавања или измене рокова прочитајте прво <a href="/manual">кратко упутство</a>.</p>
+	<form style="margin: 1em 0;" method="post" id="form">
+		<div class="formRow">
+			<div class="formRowElement">
+				<label for="subject">Предмет:</label>
 			</div>
-			<div class="formRow">
-				<label for="type" class="formRowElement">Колоквијум:</label>
-				<input type="checkbox" id="type" name="type[]" value="0" <?= ($exam->type) ? 'checked' : '' ?> class="formRowElement">
-				<div style="margin-left: auto"></div>
-				<label for="duration" class="formRowElement"><abbr title="Ако трајање није познато, унети 0">Трајање:</abbr></label>
-				<input type="number" name="duration" id="duration" value="<?= $exam->duration ?>" style="-webkit-appearance: none; -moz-appearance: textfield;">
+			<select name="subject" id="subject" class="formRowElement">
+				<?= $subjectsList ?>
+			</select>
+			<div style="margin-left: auto"></div>
+			<label for="date" class="formRowElement">Датум:</label>
+			<input type="date" name="date" id="date" value="<?= $exam->date ?>">
+		</div>
+		<div class="formRow">
+			<label for="type" class="formRowElement">Колоквијум:</label>
+			<input type="checkbox" id="type" name="type[]" value="0" <?= ($exam->type) ? 'checked' : '' ?> class="formRowElement">
+			<div style="margin-left: auto"></div>
+			<label for="duration" class="formRowElement"><abbr title="Ако трајање није познато, унети 0">Трајање:</abbr></label>
+			<input type="number" name="duration" id="duration" value="<?= $exam->duration ?>" style="-webkit-appearance: none; -moz-appearance: textfield;">
+		</div>
+		<div class="formRow">
+			<div class="formRowGroup">
+				<label for="">Модул/Смер:</label>
 			</div>
-			<div class="formRow">
-				<div class="formRowGroup">
-					<label for="">Модул/Смер:</label>
-				</div>
-				<div class="formRowGroup">
-					<input type="checkbox" name="module[]" value="0" <?= ($exam->ma) ? 'checked' : '' ?>>&nbsp;<label><abbr title="Астрономија и астрофизика">А</abbr></label>
-				</div>
-				<div class="formRowGroup">
-					<input type="checkbox" name="module[]" value="1" <?= ($exam->mi) ? 'checked' : '' ?>>&nbsp;<label><abbr title="Информатика">И</abbr></label>
-				</div>
-				<div class="formRowGroup">
-					<input type="checkbox" name="module[]" value="2" <?= ($exam->ml) ? 'checked' : '' ?>>&nbsp;<label><abbr title="Професор математике и рачунарства">Л</abbr></label>
-				</div>
-				<div class="formRowGroup">
-					<input type="checkbox" name="module[]" value="3" <?= ($exam->mm) ? 'checked' : '' ?>>&nbsp;<label><abbr title="Теоријска математика и примене">М</abbr></label>
-				</div>
-				<div class="formRowGroup">
-					<input type="checkbox" name="module[]" value="4" <?= ($exam->mp) ? 'checked' : '' ?>>&nbsp;<label><abbr title="Примењена математика">Н</abbr></label>
-				</div>
-				<div class="formRowGroup">
-					<input type="checkbox" name="module[]" value="5" <?= ($exam->mr) ? 'checked' : '' ?>>&nbsp;<label><abbr title="Рачунарство и информатика">Р</abbr></label>
-				</div>
-				<div class="formRowGroup">
-					<input type="checkbox"  name="module[]" value="6" <?= ($exam->ms) ? 'checked' : '' ?>>&nbsp;<label><abbr title="Статистика, актуарска и финансијска математика">В</abbr></label>
-				</div>
+			<div class="formRowGroup">
+				<input type="checkbox" name="module[]" value="0" <?= ($exam->ma) ? 'checked' : '' ?>>&nbsp;<label><abbr title="Астрономија и астрофизика">А</abbr></label>
 			</div>
-			<div class="formRow">
-				<label for="note">Текст:</label><br>
-				<textarea name="note" id="note" rows="5" placeholder="Ово поље садржи информације које су наведене на папиру са задацима а нису могле бити унете кроз форму."><?= $exam->note ?></textarea>
+			<div class="formRowGroup">
+				<input type="checkbox" name="module[]" value="1" <?= ($exam->mi) ? 'checked' : '' ?>>&nbsp;<label><abbr title="Информатика">И</abbr></label>
 			</div>
-			<div class="formRow" style="flex-direction: row-reverse;" id="insertProblemEntry">
-				<?php if($new): ?>
-					<div class="button smallButton" onclick="newProblemEntry()">Додај задатак</div>
-				<?php endif; ?>
+			<div class="formRowGroup">
+				<input type="checkbox" name="module[]" value="2" <?= ($exam->ml) ? 'checked' : '' ?>>&nbsp;<label><abbr title="Професор математике и рачунарства">Л</abbr></label>
 			</div>
-			<div class="formRow" style="flex-direction: row-reverse;">
-				<button type="submit" class="bigButton" onclick="window.onbeforeunload = null">Унеси</button>
+			<div class="formRowGroup">
+				<input type="checkbox" name="module[]" value="3" <?= ($exam->mm) ? 'checked' : '' ?>>&nbsp;<label><abbr title="Теоријска математика и примене">М</abbr></label>
 			</div>
-		</form>
-	</main>
+			<div class="formRowGroup">
+				<input type="checkbox" name="module[]" value="4" <?= ($exam->mp) ? 'checked' : '' ?>>&nbsp;<label><abbr title="Примењена математика">Н</abbr></label>
+			</div>
+			<div class="formRowGroup">
+				<input type="checkbox" name="module[]" value="5" <?= ($exam->mr) ? 'checked' : '' ?>>&nbsp;<label><abbr title="Рачунарство и информатика">Р</abbr></label>
+			</div>
+			<div class="formRowGroup">
+				<input type="checkbox"  name="module[]" value="6" <?= ($exam->ms) ? 'checked' : '' ?>>&nbsp;<label><abbr title="Статистика, актуарска и финансијска математика">В</abbr></label>
+			</div>
+		</div>
+		<div class="formRow">
+			<label for="note">Текст:</label><br>
+			<textarea name="note" id="note" rows="5" placeholder="Ово поље садржи информације које су наведене на папиру са задацима а нису могле бити унете кроз форму."><?= $exam->note ?></textarea>
+		</div>
+		<div class="formRow" style="flex-direction: row-reverse;" id="insertProblemEntry">
+			<?php if($new): ?>
+				<div class="button smallButton" onclick="newProblemEntry()">Додај задатак</div>
+			<?php endif; ?>
+		</div>
+		<div class="formRow" style="flex-direction: row-reverse;">
+			<button type="submit" class="bigButton" onclick="window.onbeforeunload = null">Унеси</button>
+		</div>
+	</form>
 	<script>
 		var problems = 0;
 
